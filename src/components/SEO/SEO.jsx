acc4b24c6d0a1cfad1f401cf90cd8615547134aa
Andrew Smith <andrew@andrew.codes>
2017-12-01 19:11:23 -0500

    Include Facebook Pixel support

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Helmet from "react-helmet";
+import NoScript from 'react-noscript';
 import config from "../../../data/SiteConfig";
 
 class SEO extends Component {
   render() {
-    const { postNode, postPath, postSEO } = this.props;
+    const {postNode, postPath, postSEO} = this.props;
     let title;
     let description;
     let image;
@@ -69,8 +70,8 @@ class SEO extends Component {
     return (
       <Helmet>
         {/* General tags */}
-        <meta name="description" content={description} />
-        <meta name="image" content={image} />
+        <meta name="description" content={description}/>
+        <meta name="image" content={image}/>
 
         {/* Schema.org tags */}
         <script type="application/ld+json">
@@ -78,25 +79,50 @@ class SEO extends Component {
         </script>
 
         {/* OpenGraph tags */}
-        <meta property="og:url" content={postSEO ? postURL : blogURL} />
-        {postSEO ? <meta property="og:type" content="article" /> : null}
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
+        <meta property="og:url" content={postSEO ? postURL : blogURL}/>
+        {postSEO ? <meta property="og:type" content="article"/> : null}
+        <meta property="og:title" content={title}/>
+        <meta property="og:description" content={description}/>
+        <meta property="og:image" content={image}/>
         <meta
           property="fb:app_id"
           content={config.siteFBAppID ? config.siteFBAppID : ""}
         />
 
         {/* Twitter Card tags */}
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary_large_image"/>
         <meta
           name="twitter:creator"
           content={config.userTwitter ? config.userTwitter : ""}
         />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={image} />
+        <meta name="twitter:title" content={title}/>
+        <meta name="twitter:description" content={description}/>
+        <meta name="twitter:image" content={image}/>
+        {config.siteFBInsightsID && (
+          <script>
+            {`!function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${config.siteFBInsightsID}');
+              fbq('track', 'PageView');`}
+          </script>
+        )}
+        {config.siteFBInsightsID && (
+          <NoScript>
+            <img
+              alt=""
+              height="1"
+              src={`https://www.facebook.com/tr?id=${config.siteFBInsightsID}&ev=PageView&noscript=1`}
+              style={{display: 'none'}}
+              width="1"
+            />
+          </NoScript>
+        )}
       </Helmet>
     );
   }
