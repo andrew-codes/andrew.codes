9e5bd90a58e6811c4e87c8ea09f3cc9ece9db57f
Andrew Smith <andrew@andrew.codes>
2017-12-01 19:11:00 -0500

    Update with my info

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,41 +1,35 @@
 module.exports = {
-  blogPostDir: "sample-posts", // The name of directory that contains your posts.
-  siteTitle: "Gatsby Advanced Starter", // Site title.
-  siteTitleAlt: "GatsbyJS Advanced Starter", // Alternative site title for SEO.
+  blogPostDir: "posts", // The name of directory that contains your posts.
+  siteTitle: "Andrew Smith", // Site title.
+  siteTitleAlt: "Professional profile of Andrew Smith", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-advanced-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "A GatsbyJS stater with Advanced design in mind.", // Website description used for RSS feeds/meta description tag.
+  siteUrl: "https://andrew.codes", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: "A professional profile, resume, and blog of Andrew Smith.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
-  disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
+  siteFBInsightsID: "1488033381245877", // FB Application ID for using app insights
+  googleAnalyticsID: "UA-45622906-3", // GA tracking ID.
+  disqusShortname: "andrew-codes", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
-  userName: "Advanced User", // Username to display in the author segment.
-  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
-  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
-  userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+  userName: "Andrew Smith", // Username to display in the author segment.
+  userTwitter: "andrew_codes", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Atlanta, GA", // User location to display in the author segment.
+  userAvatar: "https://www.gravatar.com/avatar/9c3d77fcdf0f8df38f39c6ef5b4cf47b", // User avatar to display in the author segment.
+  userDescription: "", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
       label: "GitHub",
-      url: "https://github.com/Vagr9K/gatsby-advanced-starter",
+      url: "https://github.com/andrew-codes",
       iconClassName: "fa fa-github"
     },
     {
       label: "Twitter",
-      url: "https://twitter.com/Vagr9K",
+      url: "https://twitter.com/andrew_codes",
       iconClassName: "fa fa-twitter"
     },
-    {
-      label: "Email",
-      url: "mailto:vagr9k@gmail.com",
-      iconClassName: "fa fa-envelope"
-    }
   ],
-  copyright: "Copyright © 2017. Advanced User", // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2017. Andrew Smith", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0" // Used for setting manifest background color.
 };
