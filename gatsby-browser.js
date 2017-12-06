d27433dd7664033ea7bcb754cecd8f9d761ed980
Andrew Smith <andrew@andrew.codes>
2017-12-06 17:45:50 -0500

    Fix issue rendering 404 page
    There is no `<main/>` tag on it and `gatsby-browser` assumes there is one.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,16 @@
 exports.onRouteUpdate = ({location}) => {
+  const mainSelector = document.querySelector('main');
+  if (!mainSelector) {
+    return;
+  }
   if (location.hash === '') {
     setTimeout(() => {
-      const scrollDoc = document.querySelector('main').parentElement;
+      const scrollDoc = mainSelector.parentElement;
       scrollDoc.scrollTo(0, 0);
     }, 0);
   } else {
     setTimeout(() => {
-      const scrollDoc = document.querySelector('main').parentElement;
+      const scrollDoc = mainSelector.parentElement;
       const hashElement = document.querySelector(location.hash);
       scrollDoc.scrollTo(0, hashElement.offsetTop);
     }, 0);
