37881a172e4bc2d22435104ee590858cbbd3bfd9
Andrew Smith <andrew@andrew.codes>
2020-10-22 14:38:09 -0400

    address issue with cycling colors

--- src/utilities/BackgroundColors.js
+++ src/utilities/BackgroundColors.js
@@ -1,63 +1,29 @@
-const { first, last } = require('lodash')
-
-class OrderedLinkedList {
-  constructor(items = []) {
-    this.items = items
-    this.current = 0
+class CylicOrderedLinkedList extends Array {
+  constructor() {
+    super()
+    this.cursor = 0
   }
 
-  add = (item) => {
-    this.items.push(item)
+  concat = (items) => {
+    const newList = new CylicOrderedLinkedList()
+    items.forEach((item) => {
+      newList.push(item)
+    })
+    return newList
   }
 
-  set = (index) => {
-    this.current = index
-  }
-
-  at = (index) => {
-    return this.items[index]
-  }
-
-  getCurrentIndex = () => {
-    return this.current
-  }
-
-  next = (item) => {
-    if (item) {
-      const index = this.items.findIndex((i) => i === item)
-      this.current = index
+  next = () => {
+    if (this.cursor === this.length) {
+      this.cursor = 0
     }
-    if (this.current === this.items.length - 1) {
-      this.current = 0
-      return first(this.items)
-    }
-    return this.items[++this.current]
-  }
-
-  nextTimes = (numberOfTimes) => {
-    for (let i = 0; i < numberOfTimes; i++) {
-      this.next()
-    }
-    return this.items[this.current]
-  }
-
-  previous = (item) => {
-    if (item) {
-      const index = this.items.findIndex((i) => i === item)
-      this.current = index
-    }
-    if (this.current === 0) {
-      return last(this.items)
-    }
-    return this.items[--this.current]
+    return this[this.cursor++]
   }
 }
 
-const BackgroundColors = new OrderedLinkedList([
+const BackgroundColors = new CylicOrderedLinkedList().concat([
   'linear-gradient(45deg, #e77842, #f7c068)',
   'linear-gradient(45deg, #b53cff, #f952ff)',
   'linear-gradient(45deg, #23bd38, #41eba9)',
-  'linear-gradient(45deg, #ffb040, #ffb040)',
   'linear-gradient(45deg, #f857a6, #ff5858)',
   'linear-gradient(45deg, #327ae7, #6bd0ff)',
 ])
